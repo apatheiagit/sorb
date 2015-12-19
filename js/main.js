4fd8c35afbf68ed04efb2345fc0b05967daf58b2
apatheiagit <apatheiar@mail.ru>
2015-12-20 01:40:43 +0300

    index

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
-
-//	
+$(document).ready(function(){
+  fixMenu();
+  $('.owl-carousel').owlCarousel({
+  	items: 1,
+  	loop: true,
+  });
+  $(window).scroll(function(){
+    fixMenu();
+  });
+  function fixMenu(){
+    var scrollTop = $(window).scrollTop();
+    if(scrollTop > 10){
+        $('.main-header').addClass('fixed');            
+    }else{
+        $('.main-header').removeClass('fixed');  
+    } 
+  }
+})
