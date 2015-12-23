4b1ab460a3cd792853cade9318680414d3ab5e4f
apatheiagit <apatheiar@mail.ru>
2015-12-23 20:24:22 +0300

    страница О компании

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
     },function(){
       $(this).children('.third-menu').hide();
   });
+  $('.right-side').height($('.somit-static').height());
 
   var options = {
       onError: function() {  console.log('ERROR'); }
